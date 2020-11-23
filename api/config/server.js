1ba4d7c616cbbc5dc73eea48d824af61422f88e0
quentin roux <quentin.roux@hotmail.fr>
2020-11-23 17:27:47 +0100

    Next Strapi api initial setup

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8cc64a96795e4d2fa97b74df4234bf63'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '40f0ffda-f624-4eb4-99bd-ec4752536e31'
+};
